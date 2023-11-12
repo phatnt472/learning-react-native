7379149184b8bbffce765c8371dc251dc3e49a73
phatnt472 <20521737@gm.uit.edu.vn>
2023-11-12 11:35:59 +0700

    first commit

--- App.js
+++ App.js
@@ -4,8 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World from React Native</Text>
       <StatusBar style="auto" />
+      <Text>HiHi</Text>
     </View>
   );
 }
